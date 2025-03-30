05893f280c798b672947b6a1db63fab87919cdd3
Caleb Lee <calee@cspire.com>
2025-03-30 04:10:14 -0500

    feat(App) - Generate Navigation and Footer Files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,21 @@
+// Angular Imports
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// Modules
+import { AppRoutingModule } from './app-routing.module';
+
+//Components
+import { AppComponent } from './app.component';
+import { NavigationBarComponent } from './layouts/navigation-bar/navigation-bar.component';
+import { FooterComponent } from './layouts/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationBarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
